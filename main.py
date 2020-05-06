78551c3052ada7dbb414e97c38ddf6da8bc11092
Dr4gon529 <fislabco23@gmail.com>
2020-05-06 20:22:57 +0900

    while 추가. 더 현실적으로

--- main.py
+++ main.py
@@ -18,17 +18,24 @@ while(True):
         choice1 = choice()
         if choice1 == 1:
             # 목소리 선택
+            # 목소리를 선택하는 화면이 나옴
             Voice = voice_choice()
             pass
 
         elif choice1 == 2:
             # 사용자 유형 선택
+            # 사용자 유형을 선택하는 화면이 나옴
             Usertype = usertype_choice()
             pass
 
         elif choice1 == 3:
             # 사용 목적 선택
+            # 사용 목적을 선택하는 화면이 나옴
             Purpose = purpose_choice()
+            pass
+
+        elif choice1 == 4:
+            # 다음 버튼을 누른 것. voice, usertype, purpose는 모두 default값이 있으니 다음 단계로 넘어감
             pass
 
     # 특정 목적지 / 학교 순회 중 선택
