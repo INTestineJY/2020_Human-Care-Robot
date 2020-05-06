13c8b5cf9d7871ebdb42cc4a5ec1895c8d91b9d2
Tochma <tochmalia@gmail.com>
2020-05-06 20:37:14 +0900

    용도별 (목적지방문/학교순회) 로봇 행동 main함수 조정

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ Voice = 1
 Usertype = 1
 Purpose = 0
 
+list_purpose1 = []
+list_purpose2 = []
+# 목적에 맞춘 목적지 순서 리스트
+# Purpose 1 은 한군데만 (리스트 안에 든 것도 하나만)
+# Purpose 2 는 학교 순회 (순회하는 곳의 순서에 맞춰 리스트에 미리 넣어두기)
 
-while(True):
+while (True):
     # start_screen()은 시작 화면을 보여주는 함수
     start_screen()
 
@@ -14,7 +19,7 @@ while(True):
     # 3 : 사용 목적 선택
     # 4 : 다음 버튼 (다음 단계로 그냥 진행. 선택하지 않고)
 
-    while(True):
+    while (True):
         choice1 = choice()
         if choice1 == 1:
             # 목소리 선택
@@ -43,15 +48,16 @@ while(True):
             break
             pass
 
-
-
     # 특정 목적지 / 학교 순회 중 선택은 purpose에서 이루어짐
     # 특정 목적지 = 1 / 학교 순회 = 2
 
-    if Purpose == 1:
-        # 특정 목적지로 안내함
-        pass
+    purpose_list = []
 
-    elif Purpose == 2:
-        # 학교내부를 순회함
+    if Purpose == 1:
+        purpose_list = list_purpose1
+    else:
+        purpose_list = list_purpose2
+
+    for purpose in purpose_list:
+        # purpose 를 가고 설명하는 함수
         pass
