e5b8351f14faa545c8cc039196e13f7d1cd116c9
Dr4gon529 <fislabco23@gmail.com>
2020-05-06 20:04:03 +0900

    앞부분

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 
 # 여기서 녹음된 목소리 / 사용자 유형 / 사용 목적 선택
 # choice는 위 세 가지를 선택하는 함수
-choice = start_screen();
+# 1 : 녹음된 목소리
+# 2 : 사용자 유형
+# 3 : 사용 목적 선택
 
+# start_screen()은 시작 화면을 보여주는 함수
+choice = start_screen()
 
+if choice == 1:
+    # 목소리 선택
+    pass
+
+elif choice == 2:
+    # 사용자 유형 선택
+    pass
+elif choice == 3:
+    # 사용 목적 선택
+    pass
 
