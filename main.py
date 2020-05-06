df783d02e7cee5018521de4b7ea7d41ec31e658e
Dr4gon529 <fislabco23@gmail.com>
2020-05-06 20:31:17 +0900

    의사코드 대략적으로 완성

--- main.py
+++ main.py
@@ -40,7 +40,10 @@ while(True):
 
         elif choice1 == 4 and Purpose != 0:
             # 다음 버튼을 누른 것. voice, usertype는 모두 default값이 있으니 다음 단계로 넘어감
+            break
             pass
+
+
 
     # 특정 목적지 / 학교 순회 중 선택은 purpose에서 이루어짐
     # 특정 목적지 = 1 / 학교 순회 = 2
