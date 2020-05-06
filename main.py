aeda542ad818b17ce317f187d66a641f2f378e28
Dr4gon529 <fislabco23@gmail.com>
2020-05-06 20:18:30 +0900

    의사코드

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
+Voice = 1
+Usertype = 1
+Purpose = 1
 
-# 여기서 녹음된 목소리 / 사용자 유형 / 사용 목적 선택
-# choice는 위 세 가지를 선택하는 함수
-# 1 : 녹음된 목소리
-# 2 : 사용자 유형
-# 3 : 사용 목적 선택
 
-# start_screen()은 시작 화면을 보여주는 함수
-choice1 = start_screen()
+while(True):
+    # 여기서 녹음된 목소리 / 사용자 유형 / 사용 목적 선택
+    # choice는 위 세 가지를 선택하는 함수
+    # 1 : 녹음된 목소리
+    # 2 : 사용자 유형
+    # 3 : 사용 목적 선택
 
-if choice1 == 1:
-    # 목소리 선택
-    pass
+    # start_screen()은 시작 화면을 보여주는 함수
+    choice1 = start_screen()
 
-elif choice1 == 2:
-    # 사용자 유형 선택
-    pass
+    if choice1 == 1:
+        # 목소리 선택
+        pass
 
-elif choice1 == 3:
-    # 사용 목적 선택
-    pass
+    elif choice1 == 2:
+        # 사용자 유형 선택
+        pass
 
-# 특정 목적지 / 학교 순회 중 선택
-# 특정 목적지 = 1 / 학교 순회 = 2
-# choose() 는 두번째 선택 함수
+    elif choice1 == 3:
+        # 사용 목적 선택
+        pass
 
-choice2 = choose()
+    # 특정 목적지 / 학교 순회 중 선택
+    # 특정 목적지 = 1 / 학교 순회 = 2
+    # choose() 는 두번째 선택 함수
 
-if choice2 == 1:
-    # 특정 목적지 입력
-    pass
-elif choice2 == 2:
-    # 학교 순회
-    pass
+    choice2 = choose()
+
+    if choice2 == 1:
+        # 특정 목적지 입력
+        pass
+    elif choice2 == 2:
+        # 학교 순회
+        pass
 
