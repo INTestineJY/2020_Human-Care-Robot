b02ceb2e0589047f80113a46bfb7ad95fe3fdd17
Dr4gon529 <fislabco23@gmail.com>
2020-05-06 20:09:09 +0900

    의사코드

--- main.py
+++ main.py
@@ -6,16 +6,28 @@
 # 3 : 사용 목적 선택
 
 # start_screen()은 시작 화면을 보여주는 함수
-choice = start_screen()
+choice1 = start_screen()
 
-if choice == 1:
+if choice1 == 1:
     # 목소리 선택
     pass
 
-elif choice == 2:
+elif choice1 == 2:
     # 사용자 유형 선택
     pass
-elif choice == 3:
+elif choice1 == 3:
     # 사용 목적 선택
+    pass
+
+# 특정 목적지 / 학교 순회 중 선택
+# 특정 목적지 = 1 / 학교 순회 = 2
+
+choice2 = choose()
+
+if choice2 == 1:
+    # 특정 목적지 입력
+    pass
+elif choice2 == 2:
+    # 학교 순회
     pass
 
