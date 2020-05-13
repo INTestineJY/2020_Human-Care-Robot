7159a7614b51375e9a5308ebe43831c444283aa9
Dr4gon529 <fislabco23@gmail.com>
2020-05-13 20:03:33 +0900

    Pyqt 공부

--- practice_deokyong.py
+++ practice_deokyong.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QGridLayout
 
 
 class MyApp(QWidget):
@@ -24,7 +24,13 @@ class MyApp(QWidget):
         vbox.addWidget(btn2)
         vbox.addWidget(btn3)
 
-        self.setLayout(vbox)
+        boxy = QGridLayout()
+        boxy.addWidget(self.btn1, 0, 0)
+        boxy.addWidget(self.btn2, 0, 1)
+        boxy.addWidget(self.btn3, 1, 0)
+
+        # self.setLayout(vbox)
+        self.setLayout(boxy)
         self.setWindowTitle('QPushButton')
         self.setGeometry(100, 100, 1080, 720)
         self.show()
