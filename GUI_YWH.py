610a2d194e25487ee047054751770a0b4f1e7710
hyun020215 <hyun020215@sasa.hs.kr>
2020-05-13 20:31:22 +0900

    시작 화면 이미지 필요

--- GUI_YWH.py
+++ GUI_YWH.py
@@ -9,5 +9,16 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        main_widget = MainWidget()
+        self.setCentralWidget(main_widget)
+
+
+class MainWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+
         start_image = QImage("./image/start_image.png")
 
+        start_window = QVBoxLayout()
+        start_window.addWidget(start_image)
+        self.setLayout(start_window)
