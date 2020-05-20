1ab5c4bbc182a62f9666606a5a19d88ae67c23c1
Tochma <tochmalia@gmail.com>
2020-05-20 19:59:57 +0900

    main 줄기 짜기 (pygame 돌리기에 필요한 설정/변수등은 추가해야 함)

--- main.py
+++ main.py
@@ -1,63 +1,67 @@
+# coding=utf-8
+import pygame
+
 Voice = 1
 Usertype = 1
 Purpose = 0
+screen = 0  # 현재 스크린 위치 (이 값에 맞추어 스크린 띄우는 화면을 설정하고, 실행되는 함수를 설정한다
+# 0: 맨 처음 화면
+# 1: 첫 선택 화면
+# 2: 목소리 선택 화면
+# 3: 사용자 유형 선택 화면
+# 10: 설명 화면
 
 list_purpose1 = []
 list_purpose2 = []
+
+
 # 목적에 맞춘 목적지 순서 리스트
 # Purpose 1 은 한군데만 (리스트 안에 든 것도 하나만)
 # Purpose 2 는 학교 순회 (순회하는 곳의 순서에 맞춰 리스트에 미리 넣어두기)
 
-while True:
-    # start_screen()은 시작 화면을 보여주는 함수
-    start_screen()
 
-    # 여기서 녹음된 목소리 / 사용자 유형 / 사용 목적 선택
-    # choice는 위 세 가지를 선택하는 함수
-    # 1 : 녹음된 목소리
-    # 2 : 사용자 유형
-    # 3 : 사용 목적 선택
-    # 4 : 다음 버튼 (다음 단계로 그냥 진행. 선택하지 않고)
+def screen1(x, y):
+    pass
 
-    while True:
-        choice1 = choice()
-        if choice1 == 1:
+
+def start_screen():
+    global screen
+    done = False
+
+    while not done:
+        clock.tick(10)
+
+        for event in pygame.event.get():  # User did something
+            if event.type == pygame.QUIT:  # If user clicked close
+                done = True
+
+        if screen == 0 :
+            # 첫 화면
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    screen = 1
+                    continue
+
+        elif screen == 1:
+            # 목소리/목적/다음화면 선택화면
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    screen1_check = screen1(event.pos[0], event.pos[1])
+
+            if screen1_check != 0:
+                screen = screen1_check
+                continue
+
+        elif screen == 2:
             # 목소리 선택
-            # 목소리를 선택하는 화면이 나옴
-            Voice = voice_choice()
             pass
 
-        elif choice1 == 2:
-            # 사용자 유형 선택
-            # 사용자 유형을 선택하는 화면이 나옴
-            Usertype = usertype_choice()
+        elif screen == 3:
+            # 목적 선택
             pass
 
-        elif choice1 == 3:
-            # 사용 목적 선택
-            # 사용 목적을 선택하는 화면이 나옴
-            Purpose = purpose_choice()
+        else:
+            # 돌아다니면서 설명할 때
             pass
 
-        elif choice1 == 4 and Purpose == 0:
-            # 사용 목적을 선택해달란 경고가 뜸
-            pass
-
-        elif choice1 == 4 and Purpose != 0:
-            # 다음 버튼을 누른 것. voice, usertype는 모두 default값이 있으니 다음 단계로 넘어감
-            break
-            pass
-
-    # 특정 목적지 / 학교 순회 중 선택은 purpose에서 이루어짐
-    # 특정 목적지 = 1 / 학교 순회 = 2
-
-    purpose_list = []
-
-    if Purpose == 1:
-        purpose_list = list_purpose1
-    else:
-        purpose_list = list_purpose2
-
-    for purpose in purpose_list:
-        # purpose 를 가고 설명하는 함수
         pass
